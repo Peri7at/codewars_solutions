79f94841e1c86f59912c8be77b4670104700e7b7
Perizat Ibraeva <perizat.ibraeva.k@gmail.com>
2022-08-07 14:31:18 +0300

    solve counting duplicates

---
+++ counting_duplicates.js
@@ -0,0 +1,38 @@
+/* Write a function that will return the count of distinct case-insensitive alphabetic
+ characters and numeric digits that occur more than once in the input string. 
+ The input string can be assumed to contain only alphabets (both uppercase and 
+ lowercase) and numeric digits. */
+
+function duplicateCount(text){
+  let count = {}
+  text.toLowerCase().split('').forEach((char)=> count[char] = (count[char] || 0) + 1);
+  let duplicateNum = 0;
+  for (const num in count) {
+    if (count[num]>1) duplicateNum++
+  };
+  return duplicateNum;
+};
+
+const readline = require('readline');
+
+process.stdin.setEncoding('utf8');
+
+let rl = readline.createInterface({
+  input: process.stdin,
+  terminal: false
+});
+
+rl.once('line', (line) => {
+  console.log(duplicateCount(line));
+  process.exit();
+});
+
+// Examples:
+
+// "abcde" -> 0  # no characters repeats more than once
+// "aabbcde" -> 2  # 'a' and 'b'
+// "aabBcde" -> 2  # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1  # 'i' occurs six times
+// "Indivisibilities" -> 2  # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2  # 'a' and '1'
+// "ABBA" -> 2  # 'A' and 'B' each occur twice
